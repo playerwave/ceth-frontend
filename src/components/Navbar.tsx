e7fb9a1cbe6c954458b13818e8ec68b230b10ccf
Thanaphat Ketsani <65160169@go.buu.ac.th>
2025-02-27 20:04:26 +0700

    change sidebar and edit layout admin_create/edit_activity

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,17 +6,19 @@ const Navbar = ({ children }) => {
   return (
     <div className="flex">
       {/* Navbar (ติดขอบบน) */}
-      <div className="fixed top-0 left-0 w-full bg-[#1E3A8A] text-white h-70px p-4 z-50">
+      <div className="fixed top-0 left-0 w-full bg-[#1E3A8A] text-white h-80px p-4 z-50">
         <h2>BUU</h2>
       </div>
 
       {/* Sidebar (ติดขอบซ้าย) */}
-      <div className="fixed top-[56px] left-0 w-[280px] h-[calc(100vh-56px)] bg-gray-800">
+      <div className="fixed top-[56px] left-0 w-[280px] h-[calc(100vh-56px)] ">
         <Sidebar />
       </div>
 
       {/* Content (เว้นที่ให้ Sidebar) */}
-      <div className="ml-[280px] mt-[56px] w-full p-4">{children}</div>
+      <div className="ml-[100px] mt-[56px] w-full p-4 flex justify-center items-center min-h-screen">
+        {children}
+      </div>
     </div>
   );
 };
