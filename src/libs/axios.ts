153e4c0b21b73b6822033ba31dbf8de6221c6c77
Natthapat Daphan <65160163@go.buu.ac.th>
2025-03-05 18:30:02 +0700

    connectFrontToBack_Activity_Admin

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -47,6 +47,10 @@ const axiosInstance: AxiosInstance = axios.create({
       ? "http://localhost:5090/api/" // ✅ แก้เป็น Backend
       : "/api",
   withCredentials: true, // ✅ ส่ง cookie ไปพร้อม request
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
 
 export default axiosInstance;
