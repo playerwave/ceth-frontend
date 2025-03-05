250638918289929c21dd1f75e420c6da24317e60
Nattanit Wongji <65160162@go.buu.ac.th>
2025-03-05 16:01:14 +0700

    editStore_ManageActivity_Admin

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -20,3 +20,13 @@ export default axiosInstance;
 // });
 
 // export default axiosInstance;
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5090/api/" // ✅ แก้เป็น Backend
+      : "/api",
+  withCredentials: true, // ✅ ส่ง cookie ไปพร้อม request
+});
+
+export default axiosInstance;
