a0c397ee617821eb7b895f4793a8d2b75fa374d9
Nattanit Wongji <65160162@go.buu.ac.th>
2025-05-17 22:57:05 +0700

    editStore_ManageActivity_Admin

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,23 +1,20 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000", // ✅ ตรวจสอบว่าใช้ `http://localhost:3000`
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-
-export default axiosInstance;
-
-// import axios, { AxiosInstance } from "axios";
-
-// const axiosInstance: AxiosInstance = axios.create({
-//   baseURL:
-//     import.meta.env.MODE === "development"
-//       ? "http://localhost:5090/api"
-//       : "/api",
-//   withCredentials: true, // ✅ ส่ง cookie ไปพร้อม request
+// const axiosInstance = axios.create({
+//   baseURL: "http://localhost:3000", // ✅ ตรวจสอบว่าใช้ `http://localhost:3000`
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
 // });
 
 // export default axiosInstance;
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5090/api/" // ✅ แก้เป็น Backend
+      : "/api",
+  withCredentials: true, // ✅ ส่ง cookie ไปพร้อม request
+});
+
+export default axiosInstance;
