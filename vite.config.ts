54d655a3f2142a38a59b7cbff50c6bdd0cbfe0a1
Thanaphat Ketsani <65160169@go.buu.ac.th>
2025-08-24 23:19:52 +0700

    fix: redirect

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  define: {
-    // ✅ กำหนด environment variables
-    __DEV__: JSON.stringify(mode === 'development'),
-    __PROD__: JSON.stringify(mode === 'production'),
-  },
-  server: {
-    port: 5173,
-    host: true,
-  },
-  preview: {
-    port: 4173,
-    host: true,
-  },
-}))
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// });
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({ plugins: [tailwindcss()], base: "/myapp" });
