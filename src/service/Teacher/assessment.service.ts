01b16e29acb3217dcbbfe7861b3e8b3b189af8b4
Thanaphat Ketsani <65160169@go.buu.ac.th>
2025-09-03 23:52:21 +0700

    fix: assessment.mapper & service

--- src/service/Teacher/assessment.service.ts
+++ src/service/Teacher/assessment.service.ts
@@ -1,10 +1,10 @@
 import axiosInstance from "../../libs/axios";
-import { ApiAssessment, ApiSetNumber } from "../../stores/api/assessment.api";
+import { ApiAssessment } from "../../stores/api/assessment.api";
 
 // 🔄 ดึงรายการแบบประเมินทั้งหมด
-export const getAllAssessments = async (): Promise<ApiSetNumber[]> => {
-  const response = await axiosInstance.get<{ data: ApiSetNumber[] }>(
-    "/teacher/setNumber/get-set-numbers"
+export const getAllAssessments = async (): Promise<ApiAssessment[]> => {
+  const response = await axiosInstance.get<{ data: ApiAssessment[] }>(
+    "/teacher/assessment/get-all-assessments"
   );
   return response.data.data; // ✅ เข้าถึง array ข้างใน
 };
