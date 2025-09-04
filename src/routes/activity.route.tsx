8f7df2da90e03447c9478398278aed3c58115ed3
Thanaphat Ketsani <65160169@go.buu.ac.th>
2025-09-04 10:56:02 +0700

    fix: qr route

--- src/routes/activity.route.tsx
+++ src/routes/activity.route.tsx
@@ -121,7 +121,7 @@ export const activityRoutes = [
     element: <ActivityCheckInOutStudent />,
     label: "ลงทะเบียนเข้าร่วมกิจกรรม",
     icon: "UserCheck",
-    roles: ["Visitor"] as RoleName[],
+    roles: ["Visitor","Teacher", "Student","Admin"] as RoleName[],
     visibleInSidebar: false,
     protectionLevel: ProtectionLevel.NONE
   }
