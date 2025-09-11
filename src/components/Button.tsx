db9d825a30abb8e2627fabb3c3284e1c4044f25e
Thanaphat Ketsani <65160169@go.buu.ac.th>
2025-09-12 04:23:20 +0700

    style: Button component change mouse style when hover button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -32,6 +32,7 @@ const Button: React.FC<ButtonProps> = ({
         "font-semibold transition duration-200 shadow-md rounded-[20px] flex items-center justify-center",
         "text-[12px] sm:text-[13px] md:text-[14px] lg:text-[16px]",
         "px-3 py-1.5 sm:px-4 sm:py-2 md:px-5 md:py-2.5 lg:px-6 lg:py-3",
+        "cursor-pointer hover:opacity-90",
         disabled && "opacity-90 cursor-not-allowed",
         className
       )}
