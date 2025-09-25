48a9d9ca594039a593bf682fba3844b4d7b8198b
Thanaphat Ketsani <65160169@go.buu.ac.th>
2025-09-26 00:48:42 +0700

    feat: add new  prop to component Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,6 +3,10 @@ import React from "react";
 interface CustomCardProps {
   height?: string | number; // Optional เพื่อให้สามารถไม่ส่งก็ได้
   width?: string | number;
+  minHeight?: string | number;
+  maxHeight?: string | number;
+  minWidth?: string | number;
+  maxWidth?: string | number;
   children: React.ReactNode;
   className?: string;
 }
@@ -10,13 +14,24 @@ interface CustomCardProps {
 const CustomCard: React.FC<CustomCardProps> = ({
   height,
   width,
+  minHeight,
+  maxHeight,
+  minWidth,
+  maxWidth,
   children,
   className,
 }) => {
   return (
     <div
-      className={`bg-white shadow-md p-4 rounded-[20px] ${className ?? ""}`}
-      style={{ height, width }}
+      className={`bg-white shadow-md p-8 rounded-[20px] w-full ${className ?? ""}`}
+      style={{ 
+        height, 
+        width: width || "100%",
+        minHeight,
+        maxHeight,
+        minWidth,
+        maxWidth
+      }}
     >
       {children}
     </div>
